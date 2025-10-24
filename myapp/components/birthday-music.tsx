469a1a9d6f00d436ef20d2fd981a433b009368ef
Calchiwo <calebwodi33@gmail.com>
2025-10-24 17:35:36 +0100

    fix

--- myapp/components/birthday-music.tsx
+++ myapp/components/birthday-music.tsx
@@ -3,7 +3,7 @@
 export default function BirthdayMusic() {
   return (
     <audio id="birthday-audio" preload="auto" loop={false}>
-      <source src="https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3" type="audio/mpeg" />
+      <source src="https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3" type="audio/mp3" />
     </audio>
   )
 }
