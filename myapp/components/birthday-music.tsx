94a490f5d30e396150f2634d808d1b090dd6fdf0
Calchiwo <calebwodi33@gmail.com>
2025-10-24 16:47:01 +0100

    Delete myapp folder from repo

--- myapp/components/birthday-music.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function BirthdayMusic() {
-  return (
-    <audio id="birthday-audio" preload="auto" loop={false}>
-      <source src="https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3" type="audio/mpeg" />
-    </audio>
-  )
-}
